3924b5c3a1442aa4595c855037036ff501d0f538
litecave <d31lthefox@gmail.com>
2022-01-11 14:58:37 +0300

    Добавьте файлы проекта.

---
+++ winsh/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace winsh
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("WinSH - Shell for Windows.");
+            Console.WriteLine("(c) litecave and contributors on GitHub, 2021.");
+            if (args.Length == 0)
+            {
+                Shell.WinSH();
+            }
+            if (args[1] == "gh")
+            {
+                Process.Start("explorer.exe", "https://github.com/litecave/winsh");
+            }
+        }
+    }
+}
---
+++ winsh/Shell.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Security.Principal;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace winsh
+{
+    public class Shell
+    {
+        static string userName = Environment.UserName;
+        public static void WinSH()
+        {
+            Console.Write("{0}> ", userName);
+            string Command = Console.ReadLine();
+        }
+    }
+}
